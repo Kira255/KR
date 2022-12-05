a38029ed585987ce976c208e38048dd1313a7865
Kira <kirakulikova255@gmail.com>
2022-12-05 21:00:40 +0300

    Задача

---
+++ Example/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] Array1 = new string[5] { "print", "num", "256", "class", "128" };
+string[] Array2 = new string[Array1.Length];
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+SecondArray(Array1, Array2);
+PrintArray(Array2);
